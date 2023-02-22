d6aff6590221a6e71ee10abbb17245787b963629
Matt Black <mblackonline.git@gmail.com>
2023-02-22 16:08:18 -0500

    Create Category.cs

---
+++ 04 Securing custom APIs/01-product-catalog-webapi-app/Models/Category.cs
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
+namespace ProductCatalog.Models
+{
+  public class Category
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
+}
